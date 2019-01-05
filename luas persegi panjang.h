bd1d25b91de4a355568d5e2b497fbc0dbf0f3d37
MuhammadEdiKurniawan01 <edikurunyawan25@gmail.com>
2019-01-05 20:43:36 +0700

    program 1

---
+++ luas persegi panjang.h
@@ -0,0 +1,16 @@
+#ifndef LUAS PERSEGI PANJANG_H
+#define LUAS PERSEGI PANJANG_H
+
+
+class luas persegi panjang
+{
+    public:
+        luas persegi panjang();
+        virtual ~luas persegi panjang();
+
+    protected:
+
+    private:
+};
+
+#endif // LUAS PERSEGI PANJANG_H
